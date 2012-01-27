698273a1a848de17fdd4f985d440e372774dde8e
kevin <kevin@33c8eb2e-3fb0-4f7a-9790-3894f54e5fb4>
2012-01-27 07:05:45 +0000

    git-svn-id: svn://192.168.1.132/autoStock/autoStock@42 33c8eb2e-3fb0-4f7a-9790-3894f54e5fb4

--- src/com/autoStock/comServer/ConnectionServer.java
+++ src/com/autoStock/comServer/ConnectionServer.java
@@ -20,73 +20,90 @@ import com.google.gson.reflect.TypeToken;
 
 /**
  * @author Kevin Kowalewski
- *
+ * 
  */
 public class ConnectionServer {
 	public static final String EndCommunication = "QUIT";
 	public static final String EndCommand = "END";
-	
-	public void startServer()
-	{
+
+	public void startServer() {
 		ServerSocket server = null;
 		Socket incoming = null;
 
-	    try {
-	    	server = new ServerSocket(8888, 5, InetAddress.getByName("127.0.0.1"));
-	    } catch (Exception e) {
-	    	e.printStackTrace();
-	    }
+		try {
+			server = new ServerSocket(8888, 5, InetAddress.getByName("127.0.0.1"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-	    while ( true )
-	    {
-	    	try {incoming = server.accept();}catch(Exception e){e.printStackTrace();}
-	    	ClientThread cs = new ClientThread( incoming );
-	    	cs.run();
-	    }
- 
-	    try {server.close();}catch(Exception e){e.printStackTrace();}
-    	//Co.println("Connection Closed. New server!");
+		while (true) {
+			try {
+				incoming = server.accept();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			ClientThread cs = new ClientThread(incoming);
+			cs.run();
+		}
+
+		try {
+			server.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		// Co.println("Connection Closed. New server!");
 	}
 
-	private class ClientThread extends Thread
-	{
+	private class ClientThread extends Thread {
 		private Socket socket;
-	    public ClientThread( Socket sock ) {
-	      super("ClientThread");
-	      socket = sock;
-	      start();
-	    }
 
-	    public void run()
-	    {
+		public ClientThread(Socket sock) {
+			super("ClientThread");
+			socket = sock;
+			start();
+		}
+
+		public void run() {
 			BufferedReader in = null;
 			PrintWriter out = null;
 			String receivedLine = new String();
 			String receivedString = new String();
 
-			try {in = new BufferedReader(new InputStreamReader(incoming.getInputStream()));}catch(Exception e){e.printStackTrace();}
-	    	try {out = new PrintWriter(incoming.getOutputStream(), true);}catch(Exception e){e.printStackTrace();}
+			try {
+				in = new BufferedReader(new InputStreamReader(incoming.getInputStream()));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			try {
+				out = new PrintWriter(incoming.getOutputStream(), true);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 
-	    	while (true)
-	    	{
-				try {receivedLine = in.readLine();}catch(IOException e){e.printStackTrace();}
-				//System.out.println("Got line: " + receivedLine);
-				if (receivedLine == null){
+			while (true) {
+				try {
+					receivedLine = in.readLine();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				// System.out.println("Got line: " + receivedLine);
+				if (receivedLine == null) {
 					break;
 				}
-				if (receivedLine.trim().equals(EndCommunication)){
-					try {server.close();}catch(IOException e){e.printStackTrace();}
+				if (receivedLine.trim().equals(EndCommunication)) {
+					try {
+						server.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
 					break;
-				}
-				else if (receivedLine.trim().equals(EndCommand)){
+				} else if (receivedLine.trim().equals(EndCommand)) {
 					new CommandReceiver().receiveGsonString(receivedString);
 					receivedString = new String();
-				}
-				else {
+				} else {
 					receivedString = receivedString.concat(receivedLine);
 				}
 			}
-	    }
+		}
 	}
 }
-
