24a11afaeb5f73ebe0f5dccd42f37a6aec5e047e
kevin <kevin@33c8eb2e-3fb0-4f7a-9790-3894f54e5fb4>
2012-02-03 06:43:48 +0000

    git-svn-id: svn://192.168.1.132/autoStock/autoStock@89 33c8eb2e-3fb0-4f7a-9790-3894f54e5fb4

--- src/com/autoStock/trading/platform/ib/IbExchangeInstance.java
+++ src/com/autoStock/trading/platform/ib/IbExchangeInstance.java
@@ -3,6 +3,9 @@
  */
 package com.autoStock.trading.platform.ib;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 import com.autoStock.Co;
 import com.autoStock.exchange.request.RequestHolder;
 import com.autoStock.trading.platform.ib.core.Contract;
@@ -38,7 +41,8 @@ public class IbExchangeInstance {
 		contract.m_symbol = typeHistoricalData.symbol;
 		contract.m_secType = "STK";
 		contract.m_currency = "USD";
-		String endDate = "20120109 10:30:00 EST";
+		//String endDate = "20120109 10:30:00 EST";
+		String endDate = new SimpleDateFormat("yyyyMMdd hh:mm:ss").format(typeHistoricalData.endDate);
 		String duration = "120 S";
 		ibExchangeClientSocket.eClientSocket.reqHistoricalData(requestHolder.requestId, contract, endDate, duration, "1 secs", "TRADES", 1, 2);
 	}
