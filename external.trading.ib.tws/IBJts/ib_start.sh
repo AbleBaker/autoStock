cf388e729ff4ce31969a0b8710a0f34c9724172e
Kevin <Kevin@33c8eb2e-3fb0-4f7a-9790-3894f54e5fb4>
2012-09-07 01:34:11 +0000

    git-svn-id: svn://192.168.1.132/autoStock/autoStock@362 33c8eb2e-3fb0-4f7a-9790-3894f54e5fb4

--- external.trading.ib.tws/IBJts/ib_start.sh
+++ external.trading.ib.tws/IBJts/ib_start.sh
@@ -1,4 +1,5 @@
-#! /bin/sh
-
 #java -cp jts.jar:hsqldb.jar:jcommon-1.0.12.jar:jfreechart-1.0.9.jar:jhall.jar:other.jar:rss.jar -Xmx512M -XX:MaxPermSize=128M jclient.LoginFrame .
-java -cp IBController.jar:jts.jar:hsqldb.jar:jfreechart-1.0.9.jar:jhall.jar:jcommon-1.0.12.jar:other.jar:rss.jar -Xmx512M -XX:MaxPermSize=128M ibcontroller.IBController
+
+while : ; do
+  java -cp IBController.jar:jts.jar:hsqldb.jar:jfreechart-1.0.9.jar:jhall.jar:jcommon-1.0.12.jar:other.jar:rss.jar -Xmx512M -XX:MaxPermSize=128M ibcontroller.IBController
+done
