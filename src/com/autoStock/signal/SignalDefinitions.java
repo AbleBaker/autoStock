323254edd8a371bacd73d534b166024ba0d4d556
Kevin <Kevin@33c8eb2e-3fb0-4f7a-9790-3894f54e5fb4>
2012-09-12 04:37:07 +0000

    git-svn-id: svn://192.168.1.132/autoStock/autoStock@380 33c8eb2e-3fb0-4f7a-9790-3894f54e5fb4

--- src/com/autoStock/signal/SignalDefinitions.java
+++ src/com/autoStock/signal/SignalDefinitions.java
@@ -43,7 +43,7 @@ public class SignalDefinitions {
 			new CalculateInterface(){@Override public int calculate(double input){return (int) (input * 1000);}},
 				20, -36, -100, -100),
 		metric_rsi(
-			new CalculateInterface(){@Override public int calculate(double input) {return (int) Math.pow(input / 2, 1.30) - 50;}},
+			new CalculateInterface(){@Override public int calculate(double input) {return (int) Math.pow(input / 2, 1.20) - 50;}},
 				30, -15, -100, -100),
 		metric_trix(
 			new CalculateInterface(){@Override public int calculate(double input) {return (int) (input * 600);}},
