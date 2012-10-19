cd03ed233b8c6c9a9889e6d8cb64aeb282db374c
Kevin <Kevin@33c8eb2e-3fb0-4f7a-9790-3894f54e5fb4>
2012-10-19 03:04:11 +0000

    git-svn-id: svn://192.168.1.132/autoStock/autoStock@498 33c8eb2e-3fb0-4f7a-9790-3894f54e5fb4

--- src/com/autoStock/MainClusteredBacktestClient.java
+++ src/com/autoStock/MainClusteredBacktestClient.java
@@ -67,7 +67,6 @@ public class MainClusteredBacktestClient implements ListenerOfCommandHolderResul
 		Co.println("--> All backtests completed...");
 		atomicIntBacktestIndex.set(0);
 		requestNextUnit();
-		System.gc();
 	}
 	
 	public void sendBacktestResult(){
