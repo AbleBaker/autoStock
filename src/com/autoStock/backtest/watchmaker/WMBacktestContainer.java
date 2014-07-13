84518355a9ba3430af9ea699d295baa9f42897b0
Kevin <Kevin@33c8eb2e-3fb0-4f7a-9790-3894f54e5fb4>
2014-07-13 07:05:19 +0000

    git-svn-id: svn://192.168.1.132/autoStock/autoStock@806 33c8eb2e-3fb0-4f7a-9790-3894f54e5fb4

--- src/com/autoStock/backtest/watchmaker/WMBacktestContainer.java
+++ src/com/autoStock/backtest/watchmaker/WMBacktestContainer.java
@@ -95,7 +95,7 @@ public class WMBacktestContainer implements EvolutionObserver<AlgorithmModel>, I
 			islandEvolutionEngine.addEvolutionObserver(this);
 			
 			if (evolutionThorough == WMEvolutionThorough.thorough_quick){
-				algorithmModel = islandEvolutionEngine.evolve(256, 8, 8, 8, new TargetFitness(999999, true), new GenerationCount(3));
+				algorithmModel = islandEvolutionEngine.evolve(512, 8, 8, 8, new TargetFitness(999999, true), new GenerationCount(8));
 			}else{
 				algorithmModel = islandEvolutionEngine.evolve(512, 16, 64, 16, new TargetFitness(999999, true), new GenerationCount(8));
 			}
@@ -167,7 +167,7 @@ public class WMBacktestContainer implements EvolutionObserver<AlgorithmModel>, I
 		
 		bestResult = data.getBestCandidateFitness();
 		
-		if (data.getGenerationNumber() != 2){
+		if (data.getGenerationNumber() != 7){
 			trainEncog(data.getBestCandidate(), historicalData);
 		}
 	}
