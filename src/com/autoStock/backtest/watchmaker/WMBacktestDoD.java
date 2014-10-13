2f2b5cdd5504c16acb852fe7d500bf48e157b2a0
Kevin <Kevin@33c8eb2e-3fb0-4f7a-9790-3894f54e5fb4>
2014-10-13 05:29:25 +0000

    Add missing file
    git-svn-id: svn://192.168.1.132/autoStock/autoStock@810 33c8eb2e-3fb0-4f7a-9790-3894f54e5fb4

---
+++ src/com/autoStock/backtest/watchmaker/WMBacktestDoD.java
@@ -0,0 +1,117 @@
+package com.autoStock.backtest.watchmaker;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import com.autoStock.Co;
+import com.autoStock.adjust.AdjustmentOfSignalMetricThreshold;
+import com.autoStock.adjust.IterableOfInteger;
+import com.autoStock.adjust.AdjustmentCampaign.AdjustmentType;
+import com.autoStock.backtest.BacktestEvaluator;
+import com.autoStock.backtest.BacktestUtils;
+import com.autoStock.database.DatabaseQuery;
+import com.autoStock.database.DatabaseDefinitions.BasicQueries;
+import com.autoStock.database.DatabaseDefinitions.QueryArg;
+import com.autoStock.database.DatabaseDefinitions.QueryArgs;
+import com.autoStock.generated.basicDefinitions.TableDefinitions.DbStockHistoricalPrice;
+import com.autoStock.indicator.results.ResultsSAR;
+import com.autoStock.internal.ApplicationStates;
+import com.autoStock.internal.Global;
+import com.autoStock.tools.Benchmark;
+import com.autoStock.tools.DateTools;
+import com.autoStock.tools.ListTools;
+import com.autoStock.trading.types.HistoricalData;
+import com.autoStock.trading.types.HistoricalDataList;
+import com.autoStock.types.Exchange;
+import com.autoStock.types.Symbol;
+import com.google.gson.internal.Pair;
+
+/**
+ * @author Kevin Kowalewski
+ *
+ */
+public class WMBacktestDoD {
+	private Exchange exchange;
+	private ArrayList<Symbol> listOfSymbols;
+	public final BacktestEvaluator backtestEvaluator = new BacktestEvaluator();
+	private ArrayList<WMBacktestContainer> listOfWMBacktestContainer = new ArrayList<WMBacktestContainer>();
+	private ArrayList<HistoricalDataList> listOfHistoricalDataList = new ArrayList<HistoricalDataList>();
+	private Date dateStart;
+	private Date dateEnd;
+	private int currentDayIndex = 0;
+	private Benchmark bench = new Benchmark();
+	private static final int DOD_RUN_PERIOD = 5;
+	private static final int DOD_TEST_PERIOD = 1;
+	
+	public WMBacktestDoD(Exchange exchange, Date dateStart, Date dateEnd, ArrayList<Symbol> listOfSymbols) {
+		this.exchange = exchange;
+		this.listOfSymbols = listOfSymbols;
+		this.dateStart = dateStart;
+		this.dateEnd = dateEnd;
+		
+		Global.callbackLock.requestLock();
+		
+		if (listOfSymbols.size() == 0){
+			throw new IllegalArgumentException("No symbols specified");
+		}
+		
+		Co.println("--> Day over Day backtest for range: " + DateTools.getPrettyDate(dateStart) + ", " + DateTools.getPrettyDate(dateEnd));
+		
+		if (DateTools.getListOfDatesOnWeekdays(dateStart, dateEnd).size() < 5){
+			throw new IllegalArgumentException("Insufficient days for DoD: " + (DateTools.getListOfDatesOnWeekdays(dateStart, dateEnd).size()));
+		}
+		
+		ListTools.removeDuplicates(listOfSymbols);
+		bench.printTick("Started");
+		
+		runStage1();
+		
+		bench.printTick("Ended");
+		Global.callbackLock.releaseLock();
+	}
+	
+	private void runStage1(){
+		for (int i=0; i<3; i++){
+			Pair <Date, Date> pair = getRunPeriodAtIndex(i);
+			
+			Co.println("--> DoD: " + pair.first + ", " + pair.second);
+			
+//			listOfHistoricalDataList = BacktestUtils.getHistoricalDataList(exchange, dateStart, dateEnd, listOfSymbols);
+//			setupBacktestContainers(pair.first, pair.second);
+//			runBacktest();
+
+		}
+	}
+	
+	private Pair<Date, Date> getRunPeriodAtIndex(int index){
+		GregorianCalendar calendarForStart = new GregorianCalendar();
+		calendarForStart.setTime(dateStart);
+		calendarForStart.roll(Calendar.DAY_OF_MONTH, index * DOD_RUN_PERIOD);
+		
+		while (DateTools.isWeekday(calendarForStart) == false){
+			calendarForStart.roll(Calendar.DAY_OF_MONTH, 1);
+		}
+		
+		GregorianCalendar calendarForEnd = (GregorianCalendar) calendarForStart.clone();
+		calendarForEnd.roll(Calendar.DAY_OF_MONTH, DOD_RUN_PERIOD-1);
+		
+		Pair<Date, Date> pair = new Pair<Date, Date>(calendarForStart.getTime(), calendarForEnd.getTime());
+		return pair;
+	}
+	
+	private void setupBacktestContainers(Date dateStart, Date dateEnd){
+		listOfWMBacktestContainer.clear();
+		for (Symbol symbol : listOfSymbols){
+			WMBacktestContainer wmBacktestContainer = new WMBacktestContainer(symbol, exchange, dateStart, dateEnd);
+			listOfWMBacktestContainer.add(wmBacktestContainer);
+		}
+	}
+	
+	public void runBacktest(){
+		for (WMBacktestContainer wmBacktestContainer : listOfWMBacktestContainer){
+			wmBacktestContainer.runBacktest();
+		}
+	}
+}
