112b3724bce075fcf5745e017d65a26510f282c7
Kevin <Kevin@33c8eb2e-3fb0-4f7a-9790-3894f54e5fb4>
2015-01-02 02:24:13 +0000

    git-svn-id: svn://192.168.1.132/autoStock/autoStock@839 33c8eb2e-3fb0-4f7a-9790-3894f54e5fb4

--- import.pl
+++ import.pl
@@ -2,7 +2,7 @@
 
 $dir = "./import";
 
-@resolutions = {"1", "5", "10", "15", "30", "60"};
+@resolutions = ["1", "5", "10", "15", "30", "60"];
 
 foreach $resolution (@resolutions){
 	print "Have resolution: $resolution";
