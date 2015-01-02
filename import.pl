145079db7c246cf05055041aa529fddf4d4d7988
Kevin <Kevin@33c8eb2e-3fb0-4f7a-9790-3894f54e5fb4>
2015-01-02 02:32:12 +0000

    git-svn-id: svn://192.168.1.132/autoStock/autoStock@845 33c8eb2e-3fb0-4f7a-9790-3894f54e5fb4

--- import.pl
+++ import.pl
@@ -18,10 +18,10 @@ foreach $resolution (@resolutions){
 
 print "*** READING DAILY BARS ***\n";
 opendir($dh, $dir);
-@files = grep { /^*(?!_)..csv$/ } readdir($dh);
+@files = grep { /^*(?!_).csv$/ } readdir($dh);
 close($dh);
 
 foreach $file (@files){
-  print "Importing: $file\n";
-#  system(qq~ mysql -pSSmxynk:: -u root autoStock -e "load data local infile '$dir/$file' into table stockHistoricalPrices fields terminated by ',' lines terminated by '\\n' ignore 1 lines (symbol, \@test, priceOpen, priceHigh, priceLow, priceClose, sizeVolume) set dateTime = str_to_date(\@test, '\%d-\%b-\%Y \%k:\%i');" ~);
+	print "Importing: $file\n";
+#    system(qq~ mysql -pSSmxynk:: -u root autoStock -e "load data local infile '$dir/$file' into table stockHistoricalPrices fields terminated by ',' lines terminated by '\\n' ignore 1 lines (symbol, \@test, priceOpen, priceHigh, priceLow, priceClose, sizeVolume) set dateTime = str_to_date(\@test, '\%d-\%b-\%Y \%k:\%i');" ~);
 }
