b48a9a99380017024b07bb11a2bf8d0c584ad55c
Kevin <Kevin@33c8eb2e-3fb0-4f7a-9790-3894f54e5fb4>
2015-01-02 02:31:43 +0000

    git-svn-id: svn://192.168.1.132/autoStock/autoStock@844 33c8eb2e-3fb0-4f7a-9790-3894f54e5fb4

--- import.pl
+++ import.pl
@@ -2,7 +2,7 @@
 
 $dir = "./import";
 
-@resolutions = ("1", "5", "10", "15", "30", "60");
+@resolutions = (); #("1", "5", "10", "15", "30", "60");
 
 foreach $resolution (@resolutions){
 	print "*** READING $resolution BARS ***\n";
@@ -16,12 +16,12 @@ foreach $resolution (@resolutions){
 	}
 }
 
-#### FINISH OFF WITH DAILY RESOLUTION of 1,440
-#opendir($dh, $dir);
-#@files = grep { /^*_$resolution.csv$/ } readdir($dh);
-#close($dh);
+print "*** READING DAILY BARS ***\n";
+opendir($dh, $dir);
+@files = grep { /^*(?!_)..csv$/ } readdir($dh);
+close($dh);
 
-#foreach $file (@files){
-#  print "Importing: $file\n";
+foreach $file (@files){
+  print "Importing: $file\n";
 #  system(qq~ mysql -pSSmxynk:: -u root autoStock -e "load data local infile '$dir/$file' into table stockHistoricalPrices fields terminated by ',' lines terminated by '\\n' ignore 1 lines (symbol, \@test, priceOpen, priceHigh, priceLow, priceClose, sizeVolume) set dateTime = str_to_date(\@test, '\%d-\%b-\%Y \%k:\%i');" ~);
-#}
+}
