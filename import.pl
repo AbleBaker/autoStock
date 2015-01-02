176e40a4bcbade799d0d328c7652c30636782a4d
Kevin <Kevin@33c8eb2e-3fb0-4f7a-9790-3894f54e5fb4>
2015-01-02 02:48:36 +0000

    git-svn-id: svn://192.168.1.132/autoStock/autoStock@864 33c8eb2e-3fb0-4f7a-9790-3894f54e5fb4

--- import.pl
+++ import.pl
@@ -22,7 +22,7 @@ opendir($dh, $dir);
 close($dh);
 
 foreach $file (@files){
-	if ($file =~ /^((?!+\_).)*$/){
+	if ($file =~ /^((?!_+).)*$/){
 		print "Importing: $file\n";
 	}
 #    system(qq~ mysql -pSSmxynk:: -u root autoStock -e "load data local infile '$dir/$file' into table stockHistoricalPrices fields terminated by ',' lines terminated by '\\n' ignore 1 lines (symbol, \@test, priceOpen, priceHigh, priceLow, priceClose, sizeVolume) set dateTime = str_to_date(\@test, '\%d-\%b-\%Y \%k:\%i');" ~);
